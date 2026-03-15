00daf8515de21f188f8bdadd01bef0b293a3a18b
kkami6 <karpacheva.kamelia@gmail.com>
2026-03-15 14:57:44 +0200

    Created the Home, Account, Flight and Reservation Controllers in the MVC

---
+++ MVC/Controllers/FlightController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class FlightController: Controller
+    {
+        private readonly FlightContext _flightContext;
+
+        public FlightController(FlightContext flightContext)
+        {
+            _flightContext = flightContext;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string departureLocation, string landingLocation,
+            DateTime departureTime, DateTime landingTime, string planeType, string planeId, string PilotName,
+            int passengerCapacity, int businessClassCapacity)
+        {
+
+            //validate
+
+            var flight = new Flight(departureLocation, landingLocation, departureTime, landingTime, planeType,
+                planeId, PilotName, passengerCapacity, businessClassCapacity);
+
+            try
+            {
+                await _flightContext.Create(flight);
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Write error: " + ex.Message);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _flightContext.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ MVC/Controllers/ReservationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace MVC.Controllers
+{
+    public class ReservationController: Controller
+    {
+        private readonly ReservationContext _reservationContext;
+
+        public ReservationController(ReservationContext reservationContext)
+        {
+            _reservationContext = reservationContext;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(string firstName, string middleName, string lastName, 
+            string personalId, string phoneNumber, string nationality, string flightType)
+        {
+            //validations
+
+            var reservation = new ReservationController(firstName, middleName, lastName, personalId, 
+                phoneNumber, nationality, flightType);
+
+            try
+            {
+                await _reservationContext.Create(reservation);
+                return RedirectToAction("Index", "Home");
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "Write error: " + ex.Message);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _reservationContext.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
